d14599f66b85a7753c4953237d249f6e5e94deb9
Denis Kornienko <korney197823@gmail.com>
2017-02-05 22:42:25 +0300

    added slick slider, image for license slider, and made icon images

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = [
-  './node_modules/normalize.css/normalize.css'
+  './node_modules/normalize.css/normalize.css',
+  './bower_components/slick-carousel/slick/slick.css',
+  './bower_components/slick-carousel/slick/slick-theme.css'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -4,4 +4,11 @@
   setTimeout(function() {
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
+
+  $('.slider').slick({
+    dots: true,
+    speed: 500,
+    //fade: true,
+    cssEase: 'linear'
+  });
 })();
